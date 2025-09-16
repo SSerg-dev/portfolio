479ab2b254e520ab38effa524a2639a701f1910f
SSerg-dev <srmail@mail.ru>
2025-09-16 11:19:03 -0400

    feat(02. header layout) header layout  done

---
+++ src/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.js-menu-toggle')
+    menuButton.onclick = function () {
+        if (menuButton.classList.contains('menu__toggle--active')) {
+            menuButton.classList.remove('menu__toggle--active')
+            menuButton.setAttribute('aria-expanded', 'false')
+        } else {
+            menuButton.classList.add('menu__toggle--active')
+            menuButton.setAttribute('aria-expanded', 'true')
+        }
+    }
+})
